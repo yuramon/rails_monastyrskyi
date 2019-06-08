4f7925ffc19486cccb01dafb78f61de2bc5322dc
yuramon <yuramon2405@gmail.com>
2019-06-08 19:58:48 +0300

    Create first rails app

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,7 +4,7 @@ class AccountsController < ApplicationController
   # GET /accounts
   # GET /accounts.json
   def index
-    @accounts = Account.all
+    @accounts = Account.all.order("age")
   end
 
   # GET /accounts/1
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_06_215109) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.integer "age"
