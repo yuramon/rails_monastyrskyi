a7fa77d74d8152cb9621eba9a21fb8d60ff25458
yuramon <yuramon2405@gmail.com>
2019-06-13 23:35:18 +0300

    add friends

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,9 +5,10 @@ class Account < ApplicationRecord
   has_many :friend_accounts, through: :friends
   has_one :cart
 
-  validates :name, presence: true, length: { minimum: 8 }, if: :name_exclude_pattern
-  validates :age, presence: true, numericality: { only_integer: true, :greater_than_or_equal_to => 18, :less_than_or_equal_to => 100 }
+  validates :name, presence: true,  if: :name_exclude_pattern
 
+  validates :age, presence: true, numericality: { only_integer: true, :greater_than_or_equal_to => 18, :less_than_or_equal_to => 100 }
+  validates_length_of :name, minimum: 8
   def name_exclude_pattern
     unless self.name !~ /^[0-9]+$/
       errors.add(:name, "invalid format.")
