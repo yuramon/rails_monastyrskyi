4df997806a32bd0c0232c9b7ac63ea8f7dd67aad
yuramon <yuramon2405@gmail.com>
2019-06-28 02:12:11 +0300

    add bootstrap and helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module ProductsHelper
+  def products_list_partial_nam
+    sum = Product.all.count
+    result = sum > 0 ? '_list' : '_empty'
+    render result.to_s
+  end
 end
